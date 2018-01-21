9d97edecbac0c1875ab1f2833dfd1e0ddb538169
sshunm <shunmugaprabhu@ymail.com>
2018-01-22 00:48:00 +0530

    nest trial query

---
+++ ElasticSearchRestAPI/ElasticSearchRestAPI/Controller/AlarmController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElasticSearchRestAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ElasticSearchRestAPI.Controller
+{
+    [Route("api/[controller]")]
+    public class AlarmController : ControllerBase
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+                .DefaultIndex("alarms");
+
+            var client = new ElasticClient(settings);
+            //return new string[] { "value1", "value2" };
+            var searchResponse = client.Search<Alarm>(s => s
+                .Query(q => q
+                    //.MatchAll()
+                    .DateRange(r => r
+                        .Field(f => f.datetime)
+                        .GreaterThanOrEquals("20190118T101000Z")
+                        )
+                )
+            );
+            return Ok(searchResponse.Documents);
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ElasticSearchRestAPI/ElasticSearchRestAPI/Model/Alarm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nest;
+
+namespace ElasticSearchRestAPI.Model
+{
+    [ElasticsearchType(Name = "alarm")]
+    public class Alarm
+    {
+        public string unit { get; set; }
+        //[Date(Format = "yyyyMMdd'T'HHmmssZ")]
+        [Date(Format = DateFormat.basic_ordinal_date_time_no_millis/*"yyyyMMdd'T'HHmmssZ"*/)]
+        public string datetime { get; set; }
+        //[Date(Format = "yyyy-MM-dd")]
+        public string date { get; set; }
+        //[Date(Format = "HH:mm:ss.SSS")]
+        public string timeofday { get; set; }
+    }
+}
